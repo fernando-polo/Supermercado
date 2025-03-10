4b893949f51d9be723c6e674aaebb870b626b649
yahircj <130490177+yahircj@users.noreply.github.com>
2025-03-10 00:44:06 -0600

    histoial compras

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PerfilController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/perfil', [PerfilController::class, 'perfil'])->name('perfil');
 Route::get('/historial-compras', [PerfilController::class, 'historial'])->name('historial');
 Route::get('/pedido/{id}', [PerfilController::class, 'detallePedido'])->name('pedido.detalle');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
